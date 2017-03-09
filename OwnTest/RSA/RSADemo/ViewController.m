8062b1eb6472095ce1e8c3a953b48a57b6055d53
RogerChen <123020990@qq.com>
2017-03-09 11:14:03 +0800

    RSA Encrypt
    A encrypt decrypt demo

---
+++ OwnTest/RSA/RSADemo/ViewController.m
@@ -0,0 +1,62 @@
+//
+//  ViewController.m
+//  RSADemo
+//
+//  Created by Roger on 17/3/9.
+//  Copyright © 2017年 Roger. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "RSAEncryptor.h"
+
+// 获取RSA密钥对 http://web.chacuo.net/netrsakeypair
+
+#define Public_Key  @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDTbZ6cNH9PgdF60aQKveLz3FTalyzHQwbp601y77SzmGHX3F5NoVUZbdK7UMdoCLK4FBziTewYD9DWvAErXZo9BFuI96bAop8wfl1VkZyyHTcznxNJFGSQd/B70/ExMgMBpEwkAAdyUqIjIdVGh1FQK/4acwS39YXwbS+IlHsPSQIDAQAB"
+#define Private_Key @"MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBANNtnpw0f0+B0XrRpAq94vPcVNqXLMdDBunrTXLvtLOYYdfcXk2hVRlt0rtQx2gIsrgUHOJN7BgP0Na8AStdmj0EW4j3psCinzB+XVWRnLIdNzOfE0kUZJB38HvT8TEyAwGkTCQAB3JSoiMh1UaHUVAr/hpzBLf1hfBtL4iUew9JAgMBAAECgYA1tGeQmAkqofga8XtwuxEWDoaDS9k0+EKeUoXGxzqoT/GyiihuIafjILFhoUA1ndf/yCQaG973sbTDhtfpMwqFNQq13+JAownslTjWgr7Hwf7qplYW92R7CU0v7wFfjqm1t/2FKU9JkHfaHfb7qqESMIbO/VMjER9o4tEx58uXDQJBAO0O4lnWDVjr1gN02cqvxPOtTY6DgFbQDeaAZF8obb6XqvCqGW/AVms3Bh8nVlUwdQ2K/xte8tHxjW9FtBQTLd8CQQDkUncO35gAqUF9Bhsdzrs7nO1J3VjLrM0ITrepqjqtVEvdXZc+1/UrkWVaIigWAXjQCVfmQzScdbznhYXPz5fXAkEAgB3KMRkhL4yNpmKRjhw+ih+ASeRCCSj6Sjfbhx4XaakYZmbXxnChg+JB+bZNz06YBFC5nLZM7y/n61o1f5/56wJBALw+ZVzE6ly5L34114uG04W9x0HcFgau7MiJphFjgUdAtd/H9xfgE4odMRPUD3q9Me9LlMYK6MiKpfm4c2+3dzcCQQC8y37NPgpNEkd9smMwPpSEjPW41aMlfcKvP4Da3z7G5bGlmuICrva9YDAiaAyDGGCK8LxC8K6HpKrFgYrXkRtt"
+
+#define RSA_File_Encrypt    0
+
+#define RSA_String_Encrypt  1
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+#if RSA_File_Encrypt
+    // 原始数据
+    NSString *originalString = @"使用openssl 文件进行加密";
+
+    // 使用.der和.p12中的公钥私钥加密解密
+    NSString *public_key_path = [[NSBundle mainBundle] pathForResource:@"public_key.der" ofType:nil];
+    NSString *private_key_path = [[NSBundle mainBundle] pathForResource:@"private_key.p12" ofType:nil];
+
+    NSString *encryptStr = [RSAEncryptor encryptString:originalString publicKeyWithContentsOfFile:public_key_path];
+    NSLog(@"加密前: %@", originalString);
+    NSLog(@"加密后: %@", encryptStr);
+    NSLog(@"解密后: %@", [RSAEncryptor decryptString:encryptStr privateKeyWithContentsOfFile:private_key_path password:@"123000"]);
+#elif RSA_String_Encrypt
+    // 原始数据
+    NSString *originalString = @"使用密钥字符串进行加密";
+    
+    // 使用字符串格式的公钥私钥加密解密
+    NSString *encryptStr = [RSAEncryptor encryptString:originalString publicKey:Public_Key];
+    
+    NSLog(@"加密前: %@", originalString);
+    NSLog(@"加密后: %@", encryptStr);
+    NSLog(@"解密后: %@", [RSAEncryptor decryptString:encryptStr privateKey:Private_Key]);
+#endif
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
